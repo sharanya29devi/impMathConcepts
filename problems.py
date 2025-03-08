cce2446ab69ff54ba5487f2795b97afb50bbb612
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2025-03-08 15:08:52 +0530

    Update problems.py

--- problems.py
+++ problems.py
@@ -6,5 +6,14 @@ while (n>0):
     l = n%10
     sum = (sum * 10) + l
     n=n//10
-return (sum==a)
+return (sum==a) #true or false
+
+#reversethenumber
+n = 123456
+r = 0
+while (n>0):
+    l = n % 10
+    r = (r*10) + l
+    n = n // 10
+print(r)
 
